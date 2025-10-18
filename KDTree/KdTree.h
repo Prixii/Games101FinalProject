01e16cb036dcb21a79d3a3da0fe63207ff05e8e6
prixii <279298470@qq.com>
2025-10-18 15:33:20 +0800

    kd Tree

--- KDTree/KdTree.h
+++ KDTree/KdTree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Eigen/Core>
 #include <execution>
+#include <numeric>
 #include <queue>
 #include <stack>
 #include <vector>
@@ -10,86 +11,314 @@ struct KdTreeNode {
   size_t point_idx_ = 0;
   int split_axis_ = 0;
   double split_threval_ = 0.f;
-  KdTreeNode* left_ = nullptr;
-  KdTreeNode* right_ = nullptr;
+  KdTreeNode *left_ = nullptr;
+  KdTreeNode *right_ = nullptr;
 
   bool IsLeaf() const { return left_ == nullptr && right_ == nullptr; }
 };
 
 struct NodeAndDistance {
-  NodeAndDistance(double distance, KdTreeNode* node)
+  NodeAndDistance(KdTreeNode *node, double distance)
       : distance_(distance), node_(node) {}
 
   double distance_ = 0.f;
-  KdTreeNode* node_ = nullptr;
+  KdTreeNode *node_ = nullptr;
 
-  bool operator<(const NodeAndDistance& other) const {
+  bool operator<(const NodeAndDistance &other) const {
     return distance_ < other.distance_;
   }
 };
 
-template <int dim>
-class KdTree {
- public:
+template <int dim> class KdTree {
+public:
   using PointType = Eigen::Matrix<double, dim, 1>;
 
   explicit KdTree() = default;
   ~KdTree() { Clear(); }
 
-  bool Build(const std::vector<PointType>& cloud);
+  bool Build(const std::vector<PointType> &cloud);
 
-  bool GetKnnPoints(const PointType& point, std::vector<size_t>& knn_idx,
+  bool GetKnnPoints(const PointType &point, std::vector<size_t> &knn_idx,
                     int k = 5);
 
-  bool GetKnnPointsMt(const std::vector<PointType>& cloud,
-                      std::vector<std::pair<size_t, size_t>>& matches,
+  bool GetKnnPointsMt(const std::vector<PointType> &cloud,
+                      std::vector<std::pair<size_t, size_t>> &matches,
                       int k = 5);
 
   void Clear();
 
-  template <int dim>
-  friend std::ostream& operator<<(std::ostream& out, const KdTree<dim>& tree) {
+  friend std::ostream &operator<<(std::ostream &out, const KdTree<dim> &tree) {
     auto root = tree.root_.get();
 
-    std::queue<KdTreeNode*> que;
+    std::queue<KdTreeNode *> que;
     que.push(root);
 
     while (!que.empty()) {
       int size = que.size();
 
       for (int i = 0; i < size; ++i) {
-        KdTreeNode* node = que.front();
+        KdTreeNode *node = que.front();
         que.pop();
 
-        if (node->is_leaf()) {
+        if (node->IsLeaf()) {
           out << "Leaf Node index: " << node->point_idx_ << std::endl;
           out << "Point Cloud: " << std::endl
-              << tree.m_cloud[node->point_idx_] << std::endl;
+              << tree.cloud_[node->point_idx_] << std::endl;
         }
 
-        if (node->left_) que.push(node->left_);
-        if (node->right_) que.push(node->right_);
+        if (node->left_)
+          que.push(node->left_);
+        if (node->right_)
+          que.push(node->right_);
       }
     }
     return out;
   }
 
- private:
-  void Search(const PointType& point,
-              std::priority_queue<NodeAndDistance>& knn_result);
+private:
+  void Search(const PointType &point,
+              std::priority_queue<NodeAndDistance> &knn_result);
 
-  bool NeedPruneBranches(const PointType& point, KdTreeNode* node,
-                         std::priority_queue<NodeAndDistance>& knn_result);
+  bool NeedPruneBranches(const PointType &point, KdTreeNode *node,
+                         std::priority_queue<NodeAndDistance> &knn_result);
 
-  void ComputeSplitParameters(const std::vector<size_t>& points_index,
-                              double& thre, int& axis,
-                              std::vector<size_t>& left,
-                              std::vector<size_t>& right);
+  void ComputeSplitParameters(const std::vector<size_t> &points_index,
+                              double &thre, int &axis,
+                              std::vector<size_t> &left,
+                              std::vector<size_t> &right);
 
-  void ComputLeafDistance(const PointType& point, KdTreeNode* node,
-                          std::priority_queue<NodeAndDistance>& knn_result);
+  void ComputLeafDistance(const PointType &point, KdTreeNode *node,
+                          std::priority_queue<NodeAndDistance> &knn_result);
 
   std::shared_ptr<KdTreeNode> root_ = nullptr;
   int k_;
   std::vector<PointType> cloud_;
 };
+
+template <int dim>
+inline bool KdTree<dim>::Build(const std::vector<PointType> &cloud) {
+  if (cloud.empty())
+    return false;
+
+  cloud_ = cloud;
+  std::vector<size_t> cloud_idx(cloud_.size());
+  std::for_each(cloud_idx.begin(), cloud_idx.end(),
+                [idx = 0](size_t &i) mutable { i = idx++; });
+
+  root_ = std::make_shared<KdTreeNode>();
+
+  std::queue<std::pair<KdTreeNode *, std::vector<size_t>>> que;
+  que.push({root_.get(), cloud_idx});
+
+  while (!que.empty()) {
+    auto size = que.size();
+    for (size_t i = 0; i < size; ++i) {
+      auto node_to_cloud = que.front();
+      que.pop();
+
+      auto node_temp = node_to_cloud.first;
+      auto cloud_idx_temp = node_to_cloud.second;
+
+      std::vector<size_t> left_cloud_idx, right_cloud_idx;
+      ComputeSplitParameters(cloud_idx_temp, node_temp->split_threval_,
+                             node_temp->split_axis_, left_cloud_idx,
+                             right_cloud_idx);
+
+      const auto create_leaf_node = [&que](const std::vector<size_t> &index,
+                                           KdTreeNode *&node) {
+        if (index.empty()) {
+          return;
+        }
+
+        node = new KdTreeNode();
+        if (index.size() == 1) {
+          node->point_idx_ = index[0];
+        } else {
+          que.push({node, index});
+        }
+      };
+
+      create_leaf_node(left_cloud_idx, node_temp->left_);
+      create_leaf_node(right_cloud_idx, node_temp->right_);
+    }
+  }
+  return true;
+}
+
+template <int dim>
+inline bool KdTree<dim>::GetKnnPoints(const PointType &point,
+                                      std::vector<size_t> &knn_idx, int k) {
+  k_ = k;
+
+  std::priority_queue<NodeAndDistance> knn_result;
+  Search(point, knn_result);
+
+  knn_idx.clear();
+
+  while (!knn_result.empty()) {
+    knn_idx.push_back(knn_result.top().node_->point_idx_);
+    knn_result.pop();
+  }
+
+  return true;
+}
+
+template <int dim>
+inline bool
+KdTree<dim>::GetKnnPointsMt(const std::vector<PointType> &cloud,
+                            std::vector<std::pair<size_t, size_t>> &matches,
+                            int k) {
+  if (cloud.empty()) {
+    return false;
+  }
+
+  matches.resize(cloud.size() * k);
+
+  std::vector<size_t> index(cloud.size());
+  std::for_each(index.begin(), index.end(),
+                [idx = 0](size_t &i) mutable { i = idx++; });
+
+  std::for_each(std::execution::par_unseq, index.begin(), index.end(),
+                [&](size_t &i) {
+                  std::vector<size_t> knn_idx;
+                  GetKnnPoints(cloud[i], knn_idx, k);
+
+                  for (size_t j = 0; j < k; ++j) {
+                    auto idx = i * k + j;
+                    matches[idx].second = knn_idx[j];
+
+                    if (j >= knn_idx.size()) {
+                      matches[idx].first = std::numeric_limits<size_t>::max();
+                    } else {
+                      matches[idx].first = i;
+                    }
+                  }
+                });
+  return true;
+}
+
+template <int dim> inline void KdTree<dim>::Clear() {
+  std::stack<KdTreeNode *> st;
+  auto root = root_.get();
+  if (root) {
+    if (root->right_) {
+      st.push(root->right_);
+    }
+    if (root->left_) {
+      st.push(root->left_);
+    }
+  }
+
+  while (!st.empty()) {
+    auto node_temp = st.top();
+    st.pop();
+
+    if (node_temp->right_) {
+      st.push(node_temp->right_);
+    }
+    if (node_temp->left_) {
+      st.push(node_temp->left_);
+    }
+    delete node_temp;
+  }
+}
+
+template <int dim>
+inline void
+KdTree<dim>::Search(const PointType &point,
+                    std::priority_queue<NodeAndDistance> &knn_result) {
+  std::stack<KdTreeNode *> st;
+  st.push(root_.get());
+
+  while (!st.empty()) {
+    auto node_temp = st.top();
+    st.pop();
+
+    if (node_temp->IsLeaf()) {
+      ComputLeafDistance(point, node_temp, knn_result);
+    } else {
+      KdTreeNode *this_side, *that_side;
+      if (point[node_temp->split_axis_] < node_temp->split_threval_) {
+        this_side = node_temp->left_;
+        that_side = node_temp->right_;
+      } else {
+        this_side = node_temp->right_;
+        that_side = node_temp->left_;
+      }
+
+      if (NeedPruneBranches(point, node_temp, knn_result)) {
+        if (that_side) {
+          st.push(that_side);
+        }
+      }
+      if (this_side) {
+        st.push(this_side);
+      }
+    }
+  }
+}
+
+template <int dim>
+inline bool KdTree<dim>::NeedPruneBranches(
+    const PointType &point, KdTreeNode *node,
+    std::priority_queue<NodeAndDistance> &knn_result) {
+  if (knn_result.size() < k_) {
+    return true;
+  }
+
+  auto dist = point[node->split_axis_] - node->split_threval_;
+  if (dist * dist < knn_result.top().distance_) {
+    return true;
+  }
+
+  return false;
+}
+
+template <int dim>
+inline void KdTree<dim>::ComputeSplitParameters(
+    const std::vector<size_t> &points_index, double &thre, int &axis,
+    std::vector<size_t> &left, std::vector<size_t> &right) {
+  using DimVector = Eigen::Matrix<double, dim, 1>;
+  DimVector means;
+  DimVector sum_of_points = std::accumulate(
+      points_index.begin(), points_index.end(), DimVector(DimVector::Zero()),
+      [&](const DimVector &sum, const size_t &index) -> DimVector {
+        return sum + cloud_[index];
+      });
+  means = sum_of_points / points_index.size();
+
+  Eigen::Matrix<double, dim, 1> vars;
+  vars = std::accumulate(points_index.begin(), points_index.end(),
+                         DimVector(DimVector::Zero()),
+                         [&](const DimVector &sum, const int &index) {
+                           auto diff = cloud_[index] - means;
+                           return (sum + diff.cwiseAbs2()).eval();
+                         });
+
+  vars.maxCoeff(&axis);
+  thre = means[axis];
+
+  std::for_each(points_index.begin(), points_index.end(),
+                [&](const size_t &idx) {
+                  if (cloud_[idx][axis] < thre) {
+                    left.push_back(idx);
+                  } else {
+                    right.push_back(idx);
+                  }
+                });
+}
+
+template <int dim>
+inline void KdTree<dim>::ComputLeafDistance(
+    const PointType &point, KdTreeNode *node,
+    std::priority_queue<NodeAndDistance> &knn_result) {
+  auto distance = (cloud_[node->point_idx_] - point).squaredNorm();
+  if (knn_result.size() < k_) {
+    knn_result.push(NodeAndDistance(node, distance));
+  } else {
+    if (distance < knn_result.top().distance_) {
+      knn_result.emplace(NodeAndDistance(node, distance));
+      knn_result.pop();
+    }
+  }
+}
--- KDTree/main.cpp
+++ KDTree/main.cpp
@@ -1,3 +1,177 @@
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <execution>
+#include <iostream>
+#include <utility>
+#include <vector>
 
+#include "Eigen/src/Core/Matrix.h"
+#include "Eigen/src/Geometry/AngleAxis.h"
+#include "KdTree.h"
 
-int main() { return 0; }
+using std::execution::par_unseq;
+
+constexpr double PI = 3.14159265358979323846;
+
+template <typename FuncT> void EvaluateAndCall(FuncT &&func, int run_num) {
+  double total_time = 0.f;
+  for (int i = 0; i < run_num; i++) {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    func();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto diff =
+        std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1)
+            .count();
+    total_time += (diff * 1000);
+  }
+  std::cout << "run cost" << total_time / 1000.f << std::endl;
+}
+
+void GenerateSphereCoordinates(std::vector<Eigen::Vector3d> &points,
+                               int num_points) {
+  points.clear();
+  double golden_ratio = (1 + std::sqrt(5)) / 2.f;
+  double angle_increment = PI * 2 * golden_ratio;
+
+  for (int i = 0; i < num_points; i++) {
+    double t = static_cast<double>(i) / num_points;
+    double inclination = std::acos(1 - 2 * t);
+    double azimuth = angle_increment * i;
+
+    double x = std::sin(inclination) * std::cos(azimuth);
+    double y = std::sin(inclination) * std::sin(azimuth);
+    double z = std::cos(inclination);
+
+    points.push_back(Eigen::Vector3d(x, y, z));
+  }
+}
+
+void TransformToEllipsoid(std::vector<Eigen::Vector3d> &points, double a,
+                          double b, double c) {
+  for (auto &point : points) {
+    point[0] *= a;
+    point[1] *= b;
+    point[2] *= c;
+  }
+}
+
+std::vector<Eigen::Vector3d> GenerateEllipsoid() {
+  double a = 200;
+  double b = 100;
+  double c = 150;
+
+  int num_points = 10000;
+
+  std::vector<Eigen::Vector3d> sphere_points;
+  GenerateSphereCoordinates(sphere_points, num_points);
+  TransformToEllipsoid(sphere_points, a, b, c);
+
+  return sphere_points;
+}
+
+void BfNn(const std::vector<Eigen::Vector3d> &source,
+          const std::vector<Eigen::Vector3d> &target,
+          std::vector<std::pair<size_t, size_t>> &result) {
+  std::vector<size_t> index(target.size());
+  for_each(index.begin(), index.end(),
+           [idx = 0](size_t &i) mutable { i = idx++; });
+  const auto bfnn_points = [&source](const Eigen::Vector3d &p) -> int {
+    return std::min_element(
+               source.begin(), source.end(),
+               [&p](const Eigen::Vector3d &p1, const Eigen::Vector3d &p2) {
+                 return (p1 - p).norm() < (p2 - p).norm();
+               }) -
+           source.begin();
+  };
+
+  result.resize(target.size());
+  for_each(par_unseq, index.begin(), index.end(), [&](size_t i) {
+    result[i].second = bfnn_points(target[i]);
+    result[i].first = i;
+  });
+}
+
+void TestKdTree3D() {
+  auto points = GenerateEllipsoid();
+
+  Eigen::AngleAxisd rvec(PI / 6, Eigen::Vector3d::UnitZ());
+
+  Eigen::Vector3d T(2, 2, 2);
+
+  std::vector<Eigen::Vector3d> source(points.size());
+
+  std::transform(points.begin(), points.end(), source.begin(),
+                 [&rvec, &T](const Eigen::Vector3d &p) {
+                   return rvec.toRotationMatrix() * p + T;
+                 });
+
+  KdTree<3> kd_tree;
+  kd_tree.Build(source);
+  std::vector<std::pair<size_t, size_t>> matches;
+  kd_tree.GetKnnPointsMt(points, matches, 1);
+
+  /*std::for_each(matches.begin(), matches.end(),
+                [](std::pair<size_t, size_t>& match) {
+                  std::cout << "Target Index: " << match.first
+                            << ", Source Index: " << match.second << std::endl;
+                });*/
+}
+
+void TestBfMatches() {
+  std::vector<Eigen::Vector3d> points = GenerateEllipsoid();
+  Eigen::AngleAxisd rvec(PI / 6, Eigen::Vector3d::UnitZ());
+  Eigen::Vector3d T(2, 2, 2);
+  std::vector<Eigen::Vector3d> source(points.size());
+  std::transform(points.begin(), points.end(), source.begin(),
+                 [&rvec, &T](const Eigen::Vector3d &p) {
+                   return rvec.toRotationMatrix() * p + T;
+                 });
+  std::vector<std::pair<size_t, size_t>> matches;
+  BfNn(source, points, matches);
+  // std::for_each(matches.begin(), matches.end(),
+  //               [](std::pair<size_t, size_t>& match) {
+  //                 std::cout << "Target Index: " << match.first
+  //                           << ", Source Index: " << match.second <<
+  //                           std::endl;
+  //               });
+}
+
+void TextKdTreeBfResult() {
+  auto points = GenerateEllipsoid();
+  Eigen::AngleAxisd rvec(PI / 6, Eigen::Vector3d::UnitZ());
+  Eigen::Vector3d T(2, 2, 2);
+  std::vector<Eigen::Vector3d> source(points.size());
+  std::transform(points.begin(), points.end(), source.begin(),
+                 [&rvec, &T](const Eigen::Vector3d &p) {
+                   return rvec.toRotationMatrix() * p + T;
+                 });
+  // kd match
+  KdTree<3> kd_tree;
+  kd_tree.Build(source);
+  std::vector<std::pair<size_t, size_t>> kd_matches;
+  kd_tree.GetKnnPointsMt(points, kd_matches, 1);
+  // bf match
+  std::vector<std::pair<size_t, size_t>> bf_matches;
+  BfNn(source, points, bf_matches);
+  //
+  size_t correct_count = 0;
+  for (size_t i = 0; i < kd_matches.size(); i++) {
+    if (kd_matches[i].second == bf_matches[i].second) {
+      correct_count++;
+    }
+  }
+  std::cout << "Correct Matches: " << correct_count << " out of "
+            << kd_matches.size() << std::endl;
+}
+
+int main() {
+  EvaluateAndCall(TestKdTree3D, 1);
+  EvaluateAndCall(TestBfMatches, 1);
+  EvaluateAndCall(TextKdTreeBfResult, 1);
+
+  return 0;
+}
